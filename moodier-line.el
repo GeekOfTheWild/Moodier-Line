9380fbe7cff790abb5255bdea1f068ab7d768be4
Geek of the Wild <thealmonddestroyer@gmail.com>
2023-11-15 18:18:00 +1100

    Add poke-line and a space after the breadcrumb thing

--- moodier-line.el
+++ moodier-line.el
@@ -829,10 +829,12 @@ Checkers checked, in order: `flycheck', `flymake'."
                      '(" "
                        (:eval (mood-line-segment-modal))
                        (:eval (mood-line-segment-buffer-status))
+                       " "
                        (:eval (breadcrumb-project-crumbs))
                        (:eval (mood-line-segment-anzu))
                        (:eval (mood-line-segment-multiple-cursors))
-                       (:eval (mood-line-segment-cursor-position))))
+                       (:eval (mood-line-segment-cursor-position))
+                       (:eval (poke-line-create))))
 
                     ;; Right
                     (format-mode-line
