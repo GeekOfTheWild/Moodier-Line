04097eb5536c3b233001b27436716e0db326b36e
Geek of the Wild <thealmonddestroyer@gmail.com>
2023-11-15 17:49:52 +1100

    Typo in previous version, fixed

--- mood-line.el
+++ mood-line.el
@@ -829,7 +829,7 @@ Checkers checked, in order: `flycheck', `flymake'."
                      '(" "
                        (:eval (mood-line-segment-modal))
                        (:eval (mood-line-segment-buffer-status))
-                       (:eval (mood-line-segment-project-crumbs))
+                       (:eval (breadcrumb-project-crumbs))
                        (:eval (mood-line-segment-anzu))
                        (:eval (mood-line-segment-multiple-cursors))
                        (:eval (mood-line-segment-cursor-position))))
